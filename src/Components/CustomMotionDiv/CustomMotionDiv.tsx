7606b469d8462de3add93b8f97b4e10673816e58
Panduss <zsuzsi.hudacsko@gmail.com>
2022-06-19 14:57:06 +0200

    now the snap is really removed

--- src/Components/CustomMotionDiv/CustomMotionDiv.tsx
+++ src/Components/CustomMotionDiv/CustomMotionDiv.tsx
@@ -1,18 +1,19 @@
-import { motion } from "framer-motion"
-import { ReactNode } from "react";
+import {motion} from "framer-motion";
+import {ReactNode} from "react";
 
 interface CustomMotionDivProps {
     duration?: number;
     ease?: string;
+    slideIn?: boolean;
     className?: string;
-    children?: ReactNode;
+    children: ReactNode;
 }
 
-const CustomMotionDiv = ({duration = 1, ease = "easeIn", className,  children}: CustomMotionDivProps): JSX.Element => {
+const CustomMotionDiv = ({duration = 1, ease = "easeIn", slideIn = false, className,  children}: CustomMotionDivProps): JSX.Element => {
     return (
         <motion.div
-            initial={{opacity: 0}}
-            animate={{opacity: 1}}
+            initial={{x: slideIn ? 100 : 0, opacity: 0, height: '100%'}}
+            whileInView={{x: 0, opacity: 1, height: '100%'}}
             transition={{ease, duration}}
             className={className}>
             {children}
