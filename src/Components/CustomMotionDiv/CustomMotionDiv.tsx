768b41642354c205d862abd399fcdeb394aa0e3a
Panduss <zsuzsi.hudacsko@gmail.com>
2022-06-19 14:11:12 +0200

    now it is working

--- src/Components/CustomMotionDiv/CustomMotionDiv.tsx
+++ src/Components/CustomMotionDiv/CustomMotionDiv.tsx
@@ -1,18 +1,19 @@
-import {motion} from "framer-motion"
+import { motion } from "framer-motion";
 import {ReactNode} from "react";
 
 interface CustomMotionDivProps {
     duration?: number;
     ease?: string;
     className?: string;
-    children?: ReactNode;
+    children: ReactNode;
 }
 
 const CustomMotionDiv = ({duration = 1, ease = "easeIn", className,  children}: CustomMotionDivProps): JSX.Element => {
+
     return (
         <motion.div
-            initial={{opacity: 0}}
-            animate={{opacity: 1}}
+            initial={{opacity: 0, height: '100%'}}
+            whileInView={{opacity: 1, height: '100%'}}
             transition={{ease, duration}}
             className={className}>
             {children}
